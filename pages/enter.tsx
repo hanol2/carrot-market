fe5456da7433e336134eab18f472e87b60fbcc64
hansol <hsring23@gmail.com>
2024-02-04 16:11:06 +0900

    style: detail page form UI

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -8,7 +8,6 @@ export default function Enter() {
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => setMethod("email");
   const onPhoneClick = () => setMethod("phone");
-  const onPhoeClick = () => setMethod("phone"); //지울것
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
