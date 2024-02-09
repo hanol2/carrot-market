4193e0cb03d8ab6dcc029ce67cccafdd997a45be
hansol <hsring23@gmail.com>
2024-02-09 21:51:04 +0900

    💄 style : Add navbar UI 구현

--- components/layout.tsx
+++ components/layout.tsx
@@ -22,13 +22,36 @@ export default function Layout({
 
   return (
     <div>
-      <div className="bg-white w-full max-w-xl text-lg font-medium py-3 fixed text-gray-800 border-b top-0 flex justify-center items-center">
-        {canGoBack ? <button onClick={onClick}>&larr;</button> : null}
+      <div
+        className={cls(
+          !canGoBack ? "justify-center" : "",
+          "bg-white w-full max-w-xl text-lg px-10 font-medium py-3 fixed text-gray-800 border-b top-0 flex items-center"
+        )}
+      >
+        {canGoBack ? (
+          <button onClick={onClick}>
+            {" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 19.5 8.25 12l7.5-7.5"
+              />
+            </svg>
+          </button>
+        ) : null}
         {title ? <span>{title}</span> : null}
       </div>
-      <div className={cls("pt-12", hasTabBar ? "pb-24" : "")}>{children}</div>
+      <div className={cls("pt-16", hasTabBar ? "pb-20" : "")}>{children}</div>
       {hasTabBar ? (
-        <nav className="bg-white max-w-xl text-gray-700 border-t fixed bottom-0 w-full px-10 pb-5 pt-3 flex justify-between text-xs">
+        <nav className="bg-white max-w-xl text-gray-800 border-t fixed bottom-0 w-full px-10 pb-5 pt-3 flex justify-between text-xs">
           <Link href="/">
             <a className="flex flex-col items-center space-y-2">
               <svg
